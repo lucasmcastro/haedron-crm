4a5b03a85f0598c3b4701a6202c3e59e94b9b4e2
Lucas Castro <lucascastro@force.local>
2010-12-22 13:44:30 -0300

    added login page and changed some layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :authenticate_user!
+  layout :specify_layout
+
+  protected 
+
+    def specify_layout
+      controller_name == 'sessions' ? 'login' : 'application' 
+    end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :lockable, :timeoutable :confirmable, and :activatable
-  devise :database_authenticatable, :registerable, 
-         :recoverable, :rememberable, :trackable, :validatable
+  # :token_authenticatable, :lockable, :timeoutable, :confirmable, :recoverable, :registerable and :activatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
