d8e60681838c21f4e75bbdba11642aa3bcfd0518
Lucas Castro <lucascastro@force.local>
2010-12-22 14:39:03 -0300

    fixed menu active class

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -25,7 +25,7 @@ class AccountsController < ApplicationController
   # GET /accounts/new.xml
   def new
     @account = Account.new
-
+        
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @account }
