a2337c6a8e3b35ad7c80798396805249057877a5
lucasmcastro <lucasmcastro@gmail.com>
2011-01-10 00:47:39 -0300

    fixed validation error display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < InheritedResources::Base
   def update
-    super do |format|
-      format.html { redirect_to users_url }
+    update! do |success, failure|
+      success.html { redirect_to users_url }
     end
   end
 
   def create
-    super do |format|
-      format.html { redirect_to users_url }
+    create! do |success, failure|
+      success.html { redirect_to users_url }
     end
   end
 end
