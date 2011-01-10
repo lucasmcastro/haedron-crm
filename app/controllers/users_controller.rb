cfadf0d988441a8587c6892d7301519b2070182c
lucasmcastro <lucasmcastro@gmail.com>
2011-01-10 00:41:25 -0300

    fixed some errors at user management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,13 @@
 class UsersController < InheritedResources::Base
-  # def update
-  #   super do |format|
-  #     format.html { redirect_to users_url }
-  #   end
-  # end
-
   def update
-    @resource = User.find_by_id(params[:id])
-    @resource.update_attributes(params[:user])
-    update! do |success, failure|
-      success.html { redirect_to users_path }
+    super do |format|
+      format.html { redirect_to users_url }
+    end
+  end
+
+  def create
+    super do |format|
+      format.html { redirect_to users_url }
     end
   end
 end
